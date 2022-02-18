500a8de1026243f37af444edb93de324c302b62a
IX <cre8tive.circle@gmail.com>
2022-02-18 11:51:22 -0800

    Program Completetion

--- src/com/letterinv/Main.java
+++ src/com/letterinv/Main.java
@@ -1,8 +1,8 @@
-//isaac cartagena
-//CS145
-//2/13/2022
-//This program takes prompts user to enter a string then counts
-//all letters in string and let you know how many are in the string
+// isaac cartagena
+// CS145
+// 2/13/2022
+// This program takes prompts user to enter a string then counts
+// all letters in string and let you know how many are in the string
 
 package com.letterinv;
 
@@ -20,55 +20,60 @@ public class Main {
 
         int[] alphaCount = new int[26];
 
-        //User input
-        System.out.println("Please enter a sentence: ");
-        String str = toLowerCase(input.nextLine());
+        try {
+            // User input
+            System.out.println("Please enter a sentence: ");
+            String str = toLowerCase(input.nextLine());
 
-        //Remove special characters
-        str = str.replaceAll("[^a-zA-Z]", " ");
+            // Remove special characters
+            str = str.replaceAll("[^a-zA-Z]", " ");
 
-        //Remove Spaces from String
-        String noSpaceStr = (str.replaceAll(" ", ""));
+            // Remove Spaces from String
+            String noSpaceStr = (str.replaceAll(" ", ""));
 
-        //loop through each letter and add up how many letters
-        //are in the user input
-        for (int i = 0; i < noSpaceStr.length(); i++) {
-            int count = 0;
-            for(char letter: alphabet) {
-                //System.out.println(count);
-                if(noSpaceStr.charAt(i) == letter) {
-                    //System.out.println("match");
-                    alphaCount[count] += 1;
+            // loop through each letter and add up how many letters
+            // are in the user input
+            for (int i = 0; i < noSpaceStr.length(); i++) {
+                int count = 0;
+                for (char letter : alphabet) {
+                    //System.out.println(count);
+                    if (noSpaceStr.charAt(i) == letter) {
+                        //System.out.println("match");
+                        alphaCount[count] += 1;
+                    }
+                    count++;
                 }
-                count++;
             }
-        }
 
-        //display totals of letters
-        System.out.println("--------Totals--------");
-        for(int i = 0; i < alphabet.length; i++) {
+            // display totals of letters
+            System.out.println("--------Totals--------");
+            for (int i = 0; i < alphabet.length; i++) {
                 System.out.print(" |" + alphabet[i] + " " + alphaCount[i] + "| ");
-                if((i+1) % 3 == 0) {
+                if ((i + 1) % 3 == 0) {
                     System.out.println(" ");
                     System.out.println("----------------------");
                 }
+            }
+
+            // find the highest amount of letters
+            int maxVal = getMaxValue(alphaCount);
+            // get index of most letters
+            int alpindex = findMax(maxVal, alphaCount);
+            System.out.println(" ");
+            System.out.println(" ");
+            // prints total letters
+            System.out.println("Total Letters: " + noSpaceStr.length());
+            System.out.println(" ");
+            // prints letter with most letters
+            System.out.println("Most Letters: " + alphabet[alpindex] + " with " + maxVal);
+        } catch (Exception e) {
+            System.out.println("There was an error");
+        } finally {
+            System.exit(0);
         }
-
-        //find the highest amount of letters
-        int maxVal = getMaxValue(alphaCount);
-        //get index of most letters
-        int alpindex = findMax(maxVal,alphaCount);
-        System.out.println(" ");
-        System.out.println(" ");
-        //prints total letters
-        System.out.println("Total Letters: " + noSpaceStr.length());
-        System.out.println(" ");
-        //prints letter with most letters
-        System.out.println("Most Letters: " + alphabet[alpindex] + " with " + maxVal);
-
     }
 
-    //get the max amount of letters
+    // get the max amount of letters
     public static int getMaxValue(int[] alphaCount){
         int maxValue = 0;
         for (int j : alphaCount) {
@@ -79,9 +84,9 @@ public class Main {
         return maxValue;
     }
 
-    //find the letter with the most letters and return its index
-    //takes the most numbers and array and returns the index of the letter
-    //if there are two letters with the same amount returns first letter
+    // find the letter with the most letters and return its index
+    // takes the most numbers and array and returns the index of the letter
+    // if there are two letters with the same amount returns first letter
     public static int findMax(int maxValue, int[] alphaCount) {
         int indexNum = 0;
         int count = 0;
